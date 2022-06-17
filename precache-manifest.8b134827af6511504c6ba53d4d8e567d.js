92264494270554898f24aa078546d465144e93ff
thounny <thounnykeo@gmail.com>
2022-06-17 17:05:18 -0400

    Updates

---
+++ precache-manifest.8b134827af6511504c6ba53d4d8e567d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6725815db43442192a0f57bf2143ec",
+    "url": "/react-chessV2/index.html"
+  },
+  {
+    "revision": "4740536d4cb808d99334",
+    "url": "/react-chessV2/static/css/main.d6d166e2.chunk.css"
+  },
+  {
+    "revision": "05d935a7a8506f7d0419",
+    "url": "/react-chessV2/static/js/2.51812d48.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/react-chessV2/static/js/2.51812d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4740536d4cb808d99334",
+    "url": "/react-chessV2/static/js/main.2d631e70.chunk.js"
+  },
+  {
+    "revision": "6a449973b4d191bbc7c5",
+    "url": "/react-chessV2/static/js/runtime-main.6a741afc.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/react-chessV2/static/media/q_w.3382a953.png"
+  }
+]);
